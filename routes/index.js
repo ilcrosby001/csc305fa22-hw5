940f994414538908371b8282715a26f584344062
ilcrosby001 <ilcrosby001@converse.edu>
2025-04-10 16:45:36 -0400

    GET serves hw5, post serves hw5 no H and a table (no form data)

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Homework 5, CSC 305 Database Design, Fall 2022' });
 });
+
+/* POST home page. */
+router.post('/', function(req, res, next) {
+  req.app.locals.formdata = req.body;
+  renderIndex(req, res, next, ' ');
+  //res.render('index', { title: ' POST' });
+});
+
+
+function renderIndex(req, res, next, title) {
+  req.app.locals.formdata = req.body; // If there's any form data, put it where it can be passed on
+  res.render('index', {title: title,
+                       formdata: req.app.locals.formdata});
+
+
+}
+
 
 module.exports = router;
